4e4c681a2bdee40e406701eb616661988aac7ef8
Sindre <Sindrees@hotmail.no>
2022-08-16 17:08:33 +0200

    Add project files.

---
+++ TableauTest/wwwroot/Viz.js
@@ -0,0 +1,14 @@
+﻿function initViz() {
+    var containerDiv = document.getElementById("vizContainer"),
+        url = "https://public.tableau.com/views/VIS_16469979919480/DashboardTotalEmployeesperYear",
+        options = {
+            hideTabs: true,
+            onFirstInteractive: function () {
+                console.log("Run this code when the viz has finished loading.");
+            }
+        };
+
+    var viz = new tableau.Viz(containerDiv, url, options);
+    // Create a viz object and embed it in the container div.
+}
+
